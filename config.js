910eb7a8b3ae5ec5aa8649712c12c4fa2815798a
mattupalligayathri <238a5a0405@risekrishnasaiprakasam.edu.in>
2025-06-28 12:01:54 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  'mongodb+srv://238a5a0405:<bOlhVbG13P81Isa>@books-store-mern.c7ba8z1.mongodb.net/<dbname>?retryWrites=true&w=Cluster0'
-
-
-
-
