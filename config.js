b0abede63bee37814b4dc8a20d60768a6ad08eb8
mattupalligayathri <238a5a0405@risekrishnasaiprakasam.edu.in>
2025-06-28 09:31:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  'mongodb+srv://238a5a0405:<bOlhVbG13P81Isa>@books-store-mern.c7ba8z1.mongodb.net/<dbname>?retryWrites=true&w=Cluster0'
+
+
+
 
