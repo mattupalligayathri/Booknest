609a7713ce146ab605803a1f5f62acf7d90f569f
mattupalligayathri <238a5a0405@risekrishnasaiprakasam.edu.in>
2025-07-02 20:41:53 +0530

    Add files via upload

--- frontend/CHANGELOG.md
+++ frontend/CHANGELOG.md
@@ -1,44 +1,234 @@
-## [v0.3.2]
-> 2016-05-13
+## 2.0.3 / 2023-05-08
 
-- **Bugfix:** Emit each deprecation warning message only once ([#24])
+### fix
 
-[v0.3.2]: https://github.com/react-bootstrap/react-prop-types/compare/v0.3.1...v0.3.2
-[#24]: https://github.com/react-bootstrap/react-prop-types/pull/24
+- treat `duration={0}` as if animation is not active by doing a check and returning early. This fixes a bug where NaN can cause a crash in the browser.
 
+## 2.0.2 / 2023-02-23
 
-## [v0.3.1]
-> 2016-05-09
+### chore
 
-- **Bugfix:** Support `isRequired` on `all` ([#22])
+- upgrade `fast-equals` to latest. No breaking changes - see https://github.com/planttheidea/fast-equals/blob/master/CHANGELOG.md
+- don't upgrade `react-transition-group` in minor release as this requires dropping support for react <16.6
+- upgrade devDependencies
+- update babel config
+- update deprecated eslint parser
 
-[v0.3.1]: https://github.com/react-bootstrap/react-prop-types/compare/v0.3.0...v0.3.1
-[#22]: https://github.com/react-bootstrap/react-prop-types/pull/22
+## 2.0.1 / 2022-06-27
 
+### feat
 
-v0.3.0 - Thu, 10 Sep 2015 19:23:24 GMT
---------------------------------------
+- feat: allow React 18
+- Remove raf polyfill for IE9
 
-- [3784da4](../../commit/3784da4) [changed] Use es6 rest for parameters of all and singlePropFrom
-- [447c243](../../commit/447c243) [fixed] 'isRequireForA11y' undefined/null checking
+## 2.0.0 / 2021-03-21
 
+### chore (#48)
 
+- Changed peerDeps to react 15,16,17
+- Removed karma,chai,enzyme blablabla... and used only Jest and Testing-Library.
+- Updated devDependencies and cleared some.
 
-v0.2.2 - Fri, 21 Aug 2015 19:22:51 GMT
---------------------------------------
+## 1.0.2 / 2018-10-02
 
-- [7d79e8d](../../commit/7d79e8d) [added] 'deprecated' wrapper for property deprecation warning
+### fix
 
+- fix babelrc
 
+## 1.0.1 / 2018-10-02
 
-v0.2.1 - Sun, 12 Jul 2015 16:04:58 GMT
---------------------------------------
+### fix
 
+- update babel, webpack, karma, etc.
+- fix import error
 
+## 1.0.0 / 2017-11-06
 
+### feat
 
+- Support React 16
 
-v0.2.0 - Sun, 12 Jul 2015 16:01:47 GMT
---------------------------------------
+## 0.1.17 / 2016-12-02
 
+### fix
 
+- change scripts
+
+## 0.1.16 / 2016-11-25
+
+### fix
+
+- update lodash
+
+## 0.1.15 / 2016-10-31
+
+### fix
+
+- fix isMounted to mounted
+
+## 0.1.14 / 2016-10-28
+
+### fix
+
+- fix: judge isMounted
+
+## 0.1.12-0.1.13 / 2016-10-27
+
+### fix
+
+- fix script
+
+## 0.1.10 / 2016-07-07
+
+### fix
+
+- add onAnimationReStart validation
+
+## 0.1.8-0.1.9 / 2016-05-05
+
+### feat
+
+- add onAniamtionStart prop
+
+## 0.1.7 / 2016-04-21
+
+### fix
+
+- fix Animate trigger animate when isActive is false
+
+## 0.1.6 / 2016-04-15
+
+### fix
+
+- fix Animate not pipe props when Animate not active
+
+## 0.1.5 / 2016-04-13
+
+### fix
+
+- remove pure-render-decorator
+
+## 0.1.4 / 2016-04-12
+
+### fix
+
+- change transition-group addons to dependencies
+
+## 0.1.3 / 2016-04-12
+
+### refactor
+
+- refactor AnimateManager
+
+## 0.1.2 / 2016-04-12
+
+### feat
+
+- use owe PureRender util
+
+### fix
+
+- update react to 15.0.0
+
+## 0.1.1 / 2016-04-05
+
+### feat
+
+- add shouldReAnimate prop
+
+## 0.1.0 / 2016-03-16
+
+### feat
+
+- use webpack 2
+
+## 0.0.13-0.0.15 / 2016-03-15
+
+### fix
+
+- using isEqual in lodash and remove isEqual in utils
+
+## 0.0.13-0.0.14 / 2016-03-15
+
+### fix
+
+- fix update animation judgement
+
+## 0.0.12 / 2016-03-15
+
+### fix
+
+- fix compatable prefix in transition property
+
+### refactor
+
+- refactor some function in utils
+- using JSX instead of createElement
+
+## 0.0.11 / 2016-03-01
+
+### fix
+
+- fix haven't unsubscribe handleStyleChange
+
+## 0.0.10 / 2016-02-17
+
+### refactor
+
+- remove lodash compose method
+- refactor configUpdate.js
+
+## 0.0.9 / 2016-02-05
+
+### fix
+
+- fix don't build on npm publish
+
+## 0.0.8 / 2016-02-05
+
+### fix
+
+- fix lodash minify problem
+
+## 0.0.7 / 2016-02-04
+
+### fix
+
+- optimize npm script commands
+
+## 0.0.6 / 2016-02-04
+
+### fix
+
+- set min time longer
+
+## 0.0.5 / 2016-02-04
+
+### fix
+
+- fix animation not valid for set css styles too quick.
+
+## 0.0.4 / 2016-02-02
+
+### fix
+
+- support onAnimationEnd in js animation
+
+## 0.0.3 / 2016-02-02
+
+### refactor
+
+- refactor the import path of lodash function
+- update webpack.config.js
+
+## 0.0.2 / 2016-02-02
+
+### feat
+
+- support js animation
+- support bezier and spring timing function
+- support group animation
+
+## 0.0.1 / 2016-01-21
+
+- Init the project
--- frontend/util.js
+++ frontend/util.js
@@ -1,594 +1,136 @@
-/* -*- Mode: js; js-indent-level: 2; -*- */
+/* eslint no-console: 0 */
+const PREFIX_LIST = ['Webkit', 'Moz', 'O', 'ms'];
+const IN_LINE_PREFIX_LIST = ['-webkit-', '-moz-', '-o-', '-ms-'];
+const IN_COMPATIBLE_PROPERTY = ['transform', 'transformOrigin', 'transition'];
+
+export const getIntersectionKeys = (preObj, nextObj) =>
+  [Object.keys(preObj), Object.keys(nextObj)].reduce((a, b) => a.filter(c => b.includes(c)));
+
+export const identity = param => param;
+
 /*
- * Copyright 2011 Mozilla Foundation and contributors
- * Licensed under the New BSD license. See LICENSE or:
- * http://opensource.org/licenses/BSD-3-Clause
+ * @description: convert camel case to dash case
+ * string => string
  */
+export const getDashCase = name => name.replace(/([A-Z])/g, v => `-${v.toLowerCase()}`);
 
-/**
- * This is a helper function for getting values from parameter/options
- * objects.
- *
- * @param args The object we are extracting values from
- * @param name The name of the property we are getting.
- * @param defaultValue An optional value to return if the property is missing
- * from the object. If this is not specified and the property is missing, an
- * error will be thrown.
+/*
+ * @description: add compatible style prefix
+ * (string, string) => object
  */
-function getArg(aArgs, aName, aDefaultValue) {
-  if (aName in aArgs) {
-    return aArgs[aName];
-  } else if (arguments.length === 3) {
-    return aDefaultValue;
-  } else {
-    throw new Error('"' + aName + '" is a required argument.');
+export const generatePrefixStyle = (name, value) => {
+  if (IN_COMPATIBLE_PROPERTY.indexOf(name) === -1) {
+    return { [name]: Number.isNaN(value) ? 0 : value };
   }
-}
-exports.getArg = getArg;
 
-var urlRegexp = /^(?:([\w+\-.]+):)?\/\/(?:(\w+:\w+)@)?([\w.-]*)(?::(\d+))?(.*)$/;
-var dataUrlRegexp = /^data:.+\,.+$/;
+  const isTransition = name === 'transition';
+  const camelName = name.replace(/(\w)/, v => v.toUpperCase());
+  let styleVal = value;
 
-function urlParse(aUrl) {
-  var match = aUrl.match(urlRegexp);
-  if (!match) {
-    return null;
-  }
-  return {
-    scheme: match[1],
-    auth: match[2],
-    host: match[3],
-    port: match[4],
-    path: match[5]
-  };
-}
-exports.urlParse = urlParse;
-
-function urlGenerate(aParsedUrl) {
-  var url = '';
-  if (aParsedUrl.scheme) {
-    url += aParsedUrl.scheme + ':';
-  }
-  url += '//';
-  if (aParsedUrl.auth) {
-    url += aParsedUrl.auth + '@';
-  }
-  if (aParsedUrl.host) {
-    url += aParsedUrl.host;
-  }
-  if (aParsedUrl.port) {
-    url += ":" + aParsedUrl.port
-  }
-  if (aParsedUrl.path) {
-    url += aParsedUrl.path;
-  }
-  return url;
-}
-exports.urlGenerate = urlGenerate;
-
-var MAX_CACHED_INPUTS = 32;
-
-/**
- * Takes some function `f(input) -> result` and returns a memoized version of
- * `f`.
- *
- * We keep at most `MAX_CACHED_INPUTS` memoized results of `f` alive. The
- * memoization is a dumb-simple, linear least-recently-used cache.
- */
-function lruMemoize(f) {
-  var cache = [];
-
-  return function(input) {
-    for (var i = 0; i < cache.length; i++) {
-      if (cache[i].input === input) {
-        var temp = cache[0];
-        cache[0] = cache[i];
-        cache[i] = temp;
-        return cache[0].result;
-      }
+  return PREFIX_LIST.reduce((result, property, i) => {
+    if (isTransition) {
+      styleVal = value.replace(/(transform|transform-origin)/gim, `${IN_LINE_PREFIX_LIST[i]}$1`);
     }
 
-    var result = f(input);
-
-    cache.unshift({
-      input,
-      result,
-    });
-
-    if (cache.length > MAX_CACHED_INPUTS) {
-      cache.pop();
-    }
-
-    return result;
-  };
-}
-
-/**
- * Normalizes a path, or the path portion of a URL:
- *
- * - Replaces consecutive slashes with one slash.
- * - Removes unnecessary '.' parts.
- * - Removes unnecessary '<dir>/..' parts.
- *
- * Based on code in the Node.js 'path' core module.
- *
- * @param aPath The path or url to normalize.
- */
-var normalize = lruMemoize(function normalize(aPath) {
-  var path = aPath;
-  var url = urlParse(aPath);
-  if (url) {
-    if (!url.path) {
-      return aPath;
-    }
-    path = url.path;
-  }
-  var isAbsolute = exports.isAbsolute(path);
-  // Split the path into parts between `/` characters. This is much faster than
-  // using `.split(/\/+/g)`.
-  var parts = [];
-  var start = 0;
-  var i = 0;
-  while (true) {
-    start = i;
-    i = path.indexOf("/", start);
-    if (i === -1) {
-      parts.push(path.slice(start));
-      break;
-    } else {
-      parts.push(path.slice(start, i));
-      while (i < path.length && path[i] === "/") {
-        i++;
-      }
-    }
-  }
-
-  for (var part, up = 0, i = parts.length - 1; i >= 0; i--) {
-    part = parts[i];
-    if (part === '.') {
-      parts.splice(i, 1);
-    } else if (part === '..') {
-      up++;
-    } else if (up > 0) {
-      if (part === '') {
-        // The first part is blank if the path is absolute. Trying to go
-        // above the root is a no-op. Therefore we can remove all '..' parts
-        // directly after the root.
-        parts.splice(i + 1, up);
-        up = 0;
-      } else {
-        parts.splice(i, 2);
-        up--;
-      }
-    }
-  }
-  path = parts.join('/');
-
-  if (path === '') {
-    path = isAbsolute ? '/' : '.';
-  }
-
-  if (url) {
-    url.path = path;
-    return urlGenerate(url);
-  }
-  return path;
-});
-exports.normalize = normalize;
-
-/**
- * Joins two paths/URLs.
- *
- * @param aRoot The root path or URL.
- * @param aPath The path or URL to be joined with the root.
- *
- * - If aPath is a URL or a data URI, aPath is returned, unless aPath is a
- *   scheme-relative URL: Then the scheme of aRoot, if any, is prepended
- *   first.
- * - Otherwise aPath is a path. If aRoot is a URL, then its path portion
- *   is updated with the result and aRoot is returned. Otherwise the result
- *   is returned.
- *   - If aPath is absolute, the result is aPath.
- *   - Otherwise the two paths are joined with a slash.
- * - Joining for example 'http://' and 'www.example.com' is also supported.
- */
-function join(aRoot, aPath) {
-  if (aRoot === "") {
-    aRoot = ".";
-  }
-  if (aPath === "") {
-    aPath = ".";
-  }
-  var aPathUrl = urlParse(aPath);
-  var aRootUrl = urlParse(aRoot);
-  if (aRootUrl) {
-    aRoot = aRootUrl.path || '/';
-  }
-
-  // `join(foo, '//www.example.org')`
-  if (aPathUrl && !aPathUrl.scheme) {
-    if (aRootUrl) {
-      aPathUrl.scheme = aRootUrl.scheme;
-    }
-    return urlGenerate(aPathUrl);
-  }
-
-  if (aPathUrl || aPath.match(dataUrlRegexp)) {
-    return aPath;
-  }
-
-  // `join('http://', 'www.example.com')`
-  if (aRootUrl && !aRootUrl.host && !aRootUrl.path) {
-    aRootUrl.host = aPath;
-    return urlGenerate(aRootUrl);
-  }
-
-  var joined = aPath.charAt(0) === '/'
-    ? aPath
-    : normalize(aRoot.replace(/\/+$/, '') + '/' + aPath);
-
-  if (aRootUrl) {
-    aRootUrl.path = joined;
-    return urlGenerate(aRootUrl);
-  }
-  return joined;
-}
-exports.join = join;
-
-exports.isAbsolute = function (aPath) {
-  return aPath.charAt(0) === '/' || urlRegexp.test(aPath);
+    return {
+      ...result,
+      [property + camelName]: styleVal,
+    };
+  }, {});
 };
 
-/**
- * Make a path relative to a URL or another path.
- *
- * @param aRoot The root path or URL.
- * @param aPath The path or URL to be made relative to aRoot.
+export const log = (...args) => {
+  console.log(...args);
+};
+
+/*
+ * @description: log the value of a varible
+ * string => any => any
  */
-function relative(aRoot, aPath) {
-  if (aRoot === "") {
-    aRoot = ".";
+export const debug = name => item => {
+  log(name, item);
+
+  return item;
+};
+
+/*
+ * @description: log name, args, return value of a function
+ * function => function
+ */
+export const debugf =
+  (tag, f) =>
+  (...args) => {
+    const res = f(...args);
+    const name = tag || f.name || 'anonymous function';
+    const argNames = `(${args.map(JSON.stringify).join(', ')})`;
+
+    log(`${name}: ${argNames} => ${JSON.stringify(res)}`);
+
+    return res;
+  };
+
+/*
+ * @description: map object on every element in this object.
+ * (function, object) => object
+ */
+export const mapObject = (fn, obj) =>
+  Object.keys(obj).reduce(
+    (res, key) => ({
+      ...res,
+      [key]: fn(key, obj[key]),
+    }),
+    {},
+  );
+
+/*
+ * @description: add compatible prefix to style
+ * object => object
+ */
+export const translateStyle = style =>
+  Object.keys(style).reduce(
+    (res, key) => ({
+      ...res,
+      ...generatePrefixStyle(key, res[key]),
+    }),
+    style,
+  );
+
+export const compose = (...args) => {
+  if (!args.length) {
+    return identity;
   }
 
-  aRoot = aRoot.replace(/\/$/, '');
+  const fns = args.reverse();
+  // first function can receive multiply arguments
+  const firstFn = fns[0];
+  const tailsFn = fns.slice(1);
 
-  // It is possible for the path to be above the root. In this case, simply
-  // checking whether the root is a prefix of the path won't work. Instead, we
-  // need to remove components from the root one by one, until either we find
-  // a prefix that fits, or we run out of components to remove.
-  var level = 0;
-  while (aPath.indexOf(aRoot + '/') !== 0) {
-    var index = aRoot.lastIndexOf("/");
-    if (index < 0) {
-      return aPath;
+  return (...composeArgs) => tailsFn.reduce((res, fn) => fn(res), firstFn(...composeArgs));
+};
+
+export const getTransitionVal = (props, duration, easing) =>
+  props.map(prop => `${getDashCase(prop)} ${duration}ms ${easing}`).join(',');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+export const warn = (condition, format, a, b, c, d, e, f) => {
+  if (isDev && typeof console !== 'undefined' && console.warn) {
+    if (format === undefined) {
+      console.warn('LogUtils requires an error message argument');
     }
 
-    // If the only part of the root that is left is the scheme (i.e. http://,
-    // file:///, etc.), one or more slashes (/), or simply nothing at all, we
-    // have exhausted all components, so the path is not relative to the root.
-    aRoot = aRoot.slice(0, index);
-    if (aRoot.match(/^([^\/]+:\/)?\/*$/)) {
-      return aPath;
-    }
+    if (!condition) {
+      if (format === undefined) {
+        console.warn(
+          'Minified exception occurred; use the non-minified dev environment ' +
+            'for the full error message and additional helpful warnings.',
+        );
+      } else {
+        const args = [a, b, c, d, e, f];
+        let argIndex = 0;
 
-    ++level;
-  }
-
-  // Make sure we add a "../" for each component we removed from the root.
-  return Array(level + 1).join("../") + aPath.substr(aRoot.length + 1);
-}
-exports.relative = relative;
-
-var supportsNullProto = (function () {
-  var obj = Object.create(null);
-  return !('__proto__' in obj);
-}());
-
-function identity (s) {
-  return s;
-}
-
-/**
- * Because behavior goes wacky when you set `__proto__` on objects, we
- * have to prefix all the strings in our set with an arbitrary character.
- *
- * See https://github.com/mozilla/source-map/pull/31 and
- * https://github.com/mozilla/source-map/issues/30
- *
- * @param String aStr
- */
-function toSetString(aStr) {
-  if (isProtoString(aStr)) {
-    return '$' + aStr;
-  }
-
-  return aStr;
-}
-exports.toSetString = supportsNullProto ? identity : toSetString;
-
-function fromSetString(aStr) {
-  if (isProtoString(aStr)) {
-    return aStr.slice(1);
-  }
-
-  return aStr;
-}
-exports.fromSetString = supportsNullProto ? identity : fromSetString;
-
-function isProtoString(s) {
-  if (!s) {
-    return false;
-  }
-
-  var length = s.length;
-
-  if (length < 9 /* "__proto__".length */) {
-    return false;
-  }
-
-  if (s.charCodeAt(length - 1) !== 95  /* '_' */ ||
-      s.charCodeAt(length - 2) !== 95  /* '_' */ ||
-      s.charCodeAt(length - 3) !== 111 /* 'o' */ ||
-      s.charCodeAt(length - 4) !== 116 /* 't' */ ||
-      s.charCodeAt(length - 5) !== 111 /* 'o' */ ||
-      s.charCodeAt(length - 6) !== 114 /* 'r' */ ||
-      s.charCodeAt(length - 7) !== 112 /* 'p' */ ||
-      s.charCodeAt(length - 8) !== 95  /* '_' */ ||
-      s.charCodeAt(length - 9) !== 95  /* '_' */) {
-    return false;
-  }
-
-  for (var i = length - 10; i >= 0; i--) {
-    if (s.charCodeAt(i) !== 36 /* '$' */) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-/**
- * Comparator between two mappings where the original positions are compared.
- *
- * Optionally pass in `true` as `onlyCompareGenerated` to consider two
- * mappings with the same original source/line/column, but different generated
- * line and column the same. Useful when searching for a mapping with a
- * stubbed out mapping.
- */
-function compareByOriginalPositions(mappingA, mappingB, onlyCompareOriginal) {
-  var cmp = strcmp(mappingA.source, mappingB.source);
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalLine - mappingB.originalLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalColumn - mappingB.originalColumn;
-  if (cmp !== 0 || onlyCompareOriginal) {
-    return cmp;
-  }
-
-  cmp = mappingA.generatedColumn - mappingB.generatedColumn;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.generatedLine - mappingB.generatedLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  return strcmp(mappingA.name, mappingB.name);
-}
-exports.compareByOriginalPositions = compareByOriginalPositions;
-
-function compareByOriginalPositionsNoSource(mappingA, mappingB, onlyCompareOriginal) {
-  var cmp
-
-  cmp = mappingA.originalLine - mappingB.originalLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalColumn - mappingB.originalColumn;
-  if (cmp !== 0 || onlyCompareOriginal) {
-    return cmp;
-  }
-
-  cmp = mappingA.generatedColumn - mappingB.generatedColumn;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.generatedLine - mappingB.generatedLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  return strcmp(mappingA.name, mappingB.name);
-}
-exports.compareByOriginalPositionsNoSource = compareByOriginalPositionsNoSource;
-
-/**
- * Comparator between two mappings with deflated source and name indices where
- * the generated positions are compared.
- *
- * Optionally pass in `true` as `onlyCompareGenerated` to consider two
- * mappings with the same generated line and column, but different
- * source/name/original line and column the same. Useful when searching for a
- * mapping with a stubbed out mapping.
- */
-function compareByGeneratedPositionsDeflated(mappingA, mappingB, onlyCompareGenerated) {
-  var cmp = mappingA.generatedLine - mappingB.generatedLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.generatedColumn - mappingB.generatedColumn;
-  if (cmp !== 0 || onlyCompareGenerated) {
-    return cmp;
-  }
-
-  cmp = strcmp(mappingA.source, mappingB.source);
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalLine - mappingB.originalLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalColumn - mappingB.originalColumn;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  return strcmp(mappingA.name, mappingB.name);
-}
-exports.compareByGeneratedPositionsDeflated = compareByGeneratedPositionsDeflated;
-
-function compareByGeneratedPositionsDeflatedNoLine(mappingA, mappingB, onlyCompareGenerated) {
-  var cmp = mappingA.generatedColumn - mappingB.generatedColumn;
-  if (cmp !== 0 || onlyCompareGenerated) {
-    return cmp;
-  }
-
-  cmp = strcmp(mappingA.source, mappingB.source);
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalLine - mappingB.originalLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalColumn - mappingB.originalColumn;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  return strcmp(mappingA.name, mappingB.name);
-}
-exports.compareByGeneratedPositionsDeflatedNoLine = compareByGeneratedPositionsDeflatedNoLine;
-
-function strcmp(aStr1, aStr2) {
-  if (aStr1 === aStr2) {
-    return 0;
-  }
-
-  if (aStr1 === null) {
-    return 1; // aStr2 !== null
-  }
-
-  if (aStr2 === null) {
-    return -1; // aStr1 !== null
-  }
-
-  if (aStr1 > aStr2) {
-    return 1;
-  }
-
-  return -1;
-}
-
-/**
- * Comparator between two mappings with inflated source and name strings where
- * the generated positions are compared.
- */
-function compareByGeneratedPositionsInflated(mappingA, mappingB) {
-  var cmp = mappingA.generatedLine - mappingB.generatedLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.generatedColumn - mappingB.generatedColumn;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = strcmp(mappingA.source, mappingB.source);
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalLine - mappingB.originalLine;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  cmp = mappingA.originalColumn - mappingB.originalColumn;
-  if (cmp !== 0) {
-    return cmp;
-  }
-
-  return strcmp(mappingA.name, mappingB.name);
-}
-exports.compareByGeneratedPositionsInflated = compareByGeneratedPositionsInflated;
-
-/**
- * Strip any JSON XSSI avoidance prefix from the string (as documented
- * in the source maps specification), and then parse the string as
- * JSON.
- */
-function parseSourceMapInput(str) {
-  return JSON.parse(str.replace(/^\)]}'[^\n]*\n/, ''));
-}
-exports.parseSourceMapInput = parseSourceMapInput;
-
-/**
- * Compute the URL of a source given the the source root, the source's
- * URL, and the source map's URL.
- */
-function computeSourceURL(sourceRoot, sourceURL, sourceMapURL) {
-  sourceURL = sourceURL || '';
-
-  if (sourceRoot) {
-    // This follows what Chrome does.
-    if (sourceRoot[sourceRoot.length - 1] !== '/' && sourceURL[0] !== '/') {
-      sourceRoot += '/';
-    }
-    // The spec says:
-    //   Line 4: An optional source root, useful for relocating source
-    //   files on a server or removing repeated values in the
-    //   “sources” entry.  This value is prepended to the individual
-    //   entries in the “source” field.
-    sourceURL = sourceRoot + sourceURL;
-  }
-
-  // Historically, SourceMapConsumer did not take the sourceMapURL as
-  // a parameter.  This mode is still somewhat supported, which is why
-  // this code block is conditional.  However, it's preferable to pass
-  // the source map URL to SourceMapConsumer, so that this function
-  // can implement the source URL resolution algorithm as outlined in
-  // the spec.  This block is basically the equivalent of:
-  //    new URL(sourceURL, sourceMapURL).toString()
-  // ... except it avoids using URL, which wasn't available in the
-  // older releases of node still supported by this library.
-  //
-  // The spec says:
-  //   If the sources are not absolute URLs after prepending of the
-  //   “sourceRoot”, the sources are resolved relative to the
-  //   SourceMap (like resolving script src in a html document).
-  if (sourceMapURL) {
-    var parsed = urlParse(sourceMapURL);
-    if (!parsed) {
-      throw new Error("sourceMapURL could not be parsed");
-    }
-    if (parsed.path) {
-      // Strip the last path component, but keep the "/".
-      var index = parsed.path.lastIndexOf('/');
-      if (index >= 0) {
-        parsed.path = parsed.path.substring(0, index + 1);
+        console.warn(format.replace(/%s/g, () => args[argIndex++]));
       }
     }
-    sourceURL = join(urlGenerate(parsed), sourceURL);
   }
-
-  return normalize(sourceURL);
-}
-exports.computeSourceURL = computeSourceURL;
+};
