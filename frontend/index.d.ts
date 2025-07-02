2bb6856ce7cbfd0e0dc8777ecd545f095d519530
mattupalligayathri <238a5a0405@risekrishnasaiprakasam.edu.in>
2025-07-02 20:16:13 +0530

    Add files via upload

--- frontend/index.d.ts
+++ frontend/index.d.ts
@@ -1,4 +1,11 @@
-import type { AcceptedPlugin, PluginCreator } from 'postcss'
+import type { PluginCreator } from 'postcss'
+import type { Config } from './config.d'
 
-declare const plugin: PluginCreator<AcceptedPlugin | string | void>
+declare const plugin: PluginCreator<string | Config | { config: string | Config }>
+
+declare type _Config = Config
+declare namespace plugin {
+  export type { _Config as Config }
+}
+
 export = plugin
